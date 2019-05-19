29f8f394aea29be08b292b86fb6500f35d819039
naorbakal <37515568+naorbak@users.noreply.github.com>
2019-05-19 10:48:35 +0300

    update: game.js

--- src/game.js
+++ src/game.js
@@ -6,7 +6,7 @@ import Deck from "./deck";
 import Player from "./player";
 import Board from './board';
 import Statistics from './statistics';
-import dominotileObj from "./dominoTileTObj";
+import dominoTileObj from "./dominoTileTObj";
 
 
 class Game extends React.Component {
@@ -14,10 +14,10 @@ class Game extends React.Component {
     constructor(props){
         super(props);
         this.dominoTilesArr = new Array();
-        createTiles();  
+        //createTiles();  
         this.state={dominoTiles: this.dominoTilesArr};
     }
-
+/*
     createTiles = ()=>{
         for(let i=0; i<=6; i++){
             for(let j=i; j<=6; j++){
@@ -29,6 +29,8 @@ class Game extends React.Component {
     chooseRandomTile= ()=>{
         return this.dominoTilesArr[Math.floor(Math.random()*this.dominoTilesArr.length)];
     }
+
+*/
     render(){
 
         return (
