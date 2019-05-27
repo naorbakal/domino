5a90cd5513ac71bbed465b5406122c714472819d
guyKremer <42372467+guyKremer@users.noreply.github.com>
2019-05-27 10:57:40 +0300

    stats ui updated

--- src/statistics.js
+++ src/statistics.js
@@ -5,6 +5,20 @@ import style from "./style.css";
 function Statistics(props){
     return(
         <div className="panel stat">
+            <div className="clock">00:00</div>
+            <div className="statLabel"> Turns So Far
+                <p>0</p>
+            </div>    
+            <div className="statLabel">Average Play Time
+                <p>0</p>
+            </div>    
+            <div className="statLabel">Withdrawals
+                <p>0</p>
+            </div>
+            <div className="statLabel">Score
+                <p>0</p>
+            </div>
+
         </div>
     )
 }
